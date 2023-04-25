20cd09b038df8817d65a8dba41a32b7feafa81e2
Lucas <lcasgalindo@gmail.com>
2023-04-25 10:55:43 -0300

    add imgbackground

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import img from './assets/aviao.jpg';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <ImageBackground source={img} resizeMode="cover" style={{flex: 1, justifyContent: 'center'}}>
+          <Text style={{fontSize: 20}}>Vaigem</Text>
+        </ImageBackground>
+      </View>
+      <View style={{flex: '70%'}}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: "30%",
+  },
+  imgBck:{
+
+  
+  },
+
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
   },
 });
